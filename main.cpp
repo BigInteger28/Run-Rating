e39070764bed9c358100026dfc5f762c119bd78a
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2021-09-20 03:04:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,61 @@ int main()
 {
     while (1 < 2)
     {
-        float distance;
-        float hours;
-        float minutes;
-        float seconds;
-        cout << "\n\nDistance in km: ";
-        cin >> distance;
-        cout << "Finish time hours: ";
-        cin >> hours;
-        cout << "Finish time minutes: ";
-        cin >> minutes;
-        cout << "Finish time seconds: ";
-        cin >> seconds;
-        float time = (hours*3600)+(minutes*60)+seconds;
-        float speed = ((distance*1000)/time)*3.6;
-        float elo = (speed*100)+(distance*50);
+        int menu = 1;
+        cout << "1. (Distance & Time known) Calculate rating" << endl;
+        cout << "2. (Rating & Distance known) Rating to kph" << endl;
+        cout << "3. (Rating & Speed known) Rating to distance" << endl;
+        cout << "Choose: ";
+        cin >> menu;
 
-        cout << "\nELO is " << int(elo) << endl;
+        if (menu == 1)
+        {
+            float distance;
+            float hours;
+            float minutes;
+            float seconds;
+            cout << "\n\nDistance in km: ";
+            cin >> distance;
+            cout << "Finish time hours: ";
+            cin >> hours;
+            cout << "Finish time minutes: ";
+            cin >> minutes;
+            cout << "Finish time seconds: ";
+            cin >> seconds;
+            float time = (hours*3600)+(minutes*60)+seconds;
+            float speed = ((distance*1000)/time)*3.6;
+            float elo = (speed*100)+(distance*50);
+
+            cout << "\nSpeed is " << speed << " kph" << endl;
+            cout << "RATING is " << int(elo) << endl;
+        }
+
+        if (menu == 2)
+        {
+            float rating;
+            float distance;
+
+            cout << "\n\nRating: ";
+            cin >> rating;
+            cout << "Distance in km: ";
+            cin >> distance;
+
+            float speed = (rating-(distance*50))/100;
+            cout << "\nSpeed is " << speed << " kph" << endl;
+        }
+
+        if (menu == 3)
+        {
+            float rating;
+            float speed;
+
+            cout << "\n\nRating: ";
+            cin >> rating;
+            cout << "Speed in kph: ";
+            cin >> speed;
+
+            float distance = (rating-(speed*100))/50;
+            cout << "\nDistance is " << distance << " km" << endl;
+        }
     }
 }
